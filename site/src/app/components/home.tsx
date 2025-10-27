243169d61e73d474fce0cc03c44f0ba352e9cc42
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-10-28 00:44:53 +0530

    setting up the home component

---
+++ site/src/app/components/home.tsx
@@ -0,0 +1,9 @@
+import { Bitcount_Grid_Single } from "next/font/google";
+const bitcountMono = Bitcount_Grid_Single({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export default function Main() {
+  return <div className="flex-1 bg-red-200">hai</div>;
+}
--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Navbar from "./components/navbar";
+import Main from "./components/home";
+
 export default function Home() {
   return (
-    <div className="min-h-screen bg-zinc-50 font-sans dark:bg-black">
+    <div className="min-h-screen bg-zinc-50 font-sans dark:bg-black flex flex-col">
       <Navbar />
-      Hi
+      <Main />
+      <Main />
     </div>
   );
 }
