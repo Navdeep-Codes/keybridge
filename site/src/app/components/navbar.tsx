a4ac1f1c190d2afb6922c2e6f5e97b1fc9cfd77c
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-10-28 00:26:02 +0530

    done with navbar

--- site/src/app/components/navbar.tsx
+++ site/src/app/components/navbar.tsx
@@ -4,15 +4,35 @@ const bitcountMono = Bitcount_Grid_Single({
   weight: ["400"],
 });
 import Link from "next/link";
+
 export default function Navbar() {
   return (
     <div>
       <nav
-        className={`${bitcountMono.className} h-10 bg-[#e8b53f] w-screen text-black text-3xl flex items-center`}
+        className={`${bitcountMono.className} h-10 bg-[#e8b53f] w-screen text-black text-3xl flex items-center justify-between px-4`}
       >
-        <Link href={"/"}>
-          <p className="pl-4">keybridge</p>
+        <Link href={"#home"}>
+          <p className="hover:underline transition-all duration-200">
+            keybridge
+          </p>
         </Link>
+        <div className="flex items-center space-x-6">
+          <Link href={"#about"}>
+            <p className="text-lg hover:underline transition-all duration-200">
+              About
+            </p>
+          </Link>
+          <Link href={"#services"}>
+            <p className="text-lg hover:underline transition-all duration-200">
+              Yubikey
+            </p>
+          </Link>
+          <Link href={"#contact"}>
+            <p className="text-lg hover:underline transition-all duration-200">
+              FAQs
+            </p>
+          </Link>
+        </div>
       </nav>
     </div>
   );
