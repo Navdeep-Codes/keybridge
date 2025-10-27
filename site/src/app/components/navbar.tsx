d69b405802e36ee2210e5abdb117cd9dacb06ef7
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-10-28 00:00:22 +0530

    basic navbar

--- site/src/app/components/navbar.tsx
+++ site/src/app/components/navbar.tsx
@@ -1,7 +1,19 @@
+import { Bitcount_Grid_Single } from "next/font/google";
+const bitcountMono = Bitcount_Grid_Single({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+import Link from "next/link";
 export default function Navbar() {
   return (
     <div>
-      <nav className="h-20 bg-[#e8b53f] w-screen">hi</nav>
+      <nav
+        className={`${bitcountMono.className} h-10 bg-[#e8b53f] w-screen text-black text-3xl flex items-center`}
+      >
+        <Link href={"/"}>
+          <p className="pl-4">keybridge</p>
+        </Link>
+      </nav>
     </div>
   );
 }
